3b06817eb2c5e6b1a044626f67d1e2865cdba64c
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-05-18 02:57:49 -0700

    Initial commit.

---
+++ basic_oop_with_python/7-main.py
@@ -0,0 +1,9 @@
+from number import Number
+import math
+
+n4 = Number(20)
+n5 = Number(5)
+n1 = Number(1)
+
+n1.set_value(n4 / n5)
+print n1
---
+++ basic_oop_with_python/number.py
@@ -0,0 +1,26 @@
+import math
+
+"""Class"""
+class Number(object):
+    """Constructs a number."""
+    def __init__(self, value):
+        self.__value = value
+
+    def set_value(self, sum):
+        self.__value = sum
+
+    """Redefine operators."""
+    def __add__(self,other):
+        return Number(self.__value + other.__value)
+
+    def __sub__(self,other):
+        return Number(self.__value - other.__value)
+
+    def __mul__(self,other):
+        return Number(self.__value * other.__value)
+
+    def __div__(self,other):
+        return Number(self.__value / other.__value)
+
+    def __str__(self):
+        return str(self.__value)
