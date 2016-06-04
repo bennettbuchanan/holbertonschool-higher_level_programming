fb8dbb01719572ebcad8c51dcfa4a425e15655c8
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-06-03 17:57:28 -0700

    Initial commit.

---
+++ xml_and_json_are_the_base_of_web_2_0/5-main.py
@@ -0,0 +1,55 @@
+import urllib
+import json
+from xml.dom.minidom import Document
+from car import Car
+
+"""Load data from the URL and write to a file."""
+url = "http://173.246.108.142/oop/cars.json"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+target = open("5-main.json", 'w')
+list_dump = json.dumps(data, sort_keys=True, indent=4,
+                       separators=(',', ': '))
+target.write(list_dump)
+target.close()
+
+with open("5-main.json") as json_file:
+    data_file = json.load(json_file)
+json_file.close()
+
+i = 0
+objArray = []
+for obj in data_file:
+    c = Car(name=str(obj['name']), brand=str(obj['brand']),
+            nb_doors=obj['nb_doors'])
+    objArray.append(c)
+
+"""Add the first name to the brandArray, since it must be unique."""
+brandArray = [objArray[0].get_brand()]
+
+k = 1
+totalDoors = 0
+doc = Document()
+rootNode = doc.createElement("cars")
+"""Traverse the objArray to determine existence in the brandArray."""
+for i in objArray:
+    for j in brandArray:
+        if i.get_brand() == j:
+            k = 1
+            break
+        if k == len(brandArray):
+            brandArray.append(i.get_brand())
+            k = 1
+            break
+        k += 1
+    totalDoors += i.get_nb_doors()
+
+    carNode = i.to_xml_node(doc)
+    rootNode.appendChild(carNode)
+
+doc.appendChild(rootNode)
+
+print len(brandArray)
+print totalDoors
+print objArray[3]
+print doc.toxml(encoding='utf-8')
---
+++ xml_and_json_are_the_base_of_web_2_0/car.py
@@ -0,0 +1,78 @@
+import json
+
+class Car(object):
+    def __init__(self, *args, **kwargs):
+        """Function description.
+        Keyword arguments:
+        *args -- A tuple of arguments passed.
+        **kwargs -- A dictionary of arguments.
+        """
+
+        if len(args) == 0:
+            if type(kwargs['name']) is not str or len(kwargs['name']) is 0:
+                raise Exception("name is not a string")
+            if type(kwargs['brand']) is not str or len(kwargs['brand']) is 0:
+                raise Exception("brand is not a string")
+            if type(kwargs['nb_doors']) is not int or kwargs['nb_doors'] <= 0:
+                raise Exception("nb_doors is not > 0")
+            self.__name = kwargs['name']
+            self.__brand = kwargs['brand']
+            self.__nb_doors = kwargs['nb_doors']
+        else:
+            if type(args[0]['name']) is not str or len(args[0]['name']) is 0:
+                raise Exception("name is not a string")
+            if type(args[0]['brand']) is not str or len(args[0]['brand']) is 0:
+                raise Exception("brand is not a string")
+            if type(args[0]['nb_doors']) is not int or args[0]['nb_doors'] <= 0:
+                raise Exception("nb_doors is not > 0")
+            self.__name = args[0]['name']
+            self.__brand = args[0]['brand']
+            self.__nb_doors = args[0]['nb_doors']
+
+    def get_name(self):
+        """Return get_name."""
+        return self.__name
+
+    def get_brand(self):
+        """Return get_brand."""
+        return self.__brand
+
+    def get_nb_doors(self):
+        """Return get_nb_doors."""
+        return self.__nb_doors
+
+    def set_nb_doors(self, number):
+        """Update the private attribute nb_doors."""
+        if type(number) is not int or number <= 0:
+            raise Exception("nb_doors is not > 0")
+        self.__nb_doors = number
+
+    def to_hash(self):
+        """Return a hash of arguments passed."""
+        hash = {
+            "name": self.__name,
+            "brand": self.__brand,
+            "nb_doors": self.__nb_doors
+        }
+        return hash
+
+    def to_json_string(self):
+        """Return a string of the class in JSON format."""
+        str = self.to_hash()
+        return json.dumps(str)
+
+    def to_xml_node(self, doc):
+        """Return an XML DOM element with the attributes and values."""
+        carNode = doc.createElement("car")
+        carNode.setAttribute("nb_doors", str(self.__nb_doors))
+        nameNode = doc.createElement("name")
+        carNode.appendChild(nameNode)
+        nameNode.appendChild(doc.createCDATASection(self.__name))
+        brandNode = doc.createElement("brand")
+        carNode.appendChild(brandNode)
+        brandNode.appendChild(doc.createTextNode(self.__brand))
+        return carNode
+
+    def __str__(self):
+        """Return the attributes of this class."""
+        return self.__name + " " + self.__brand + " (" + str(self.__nb_doors) + ")"
