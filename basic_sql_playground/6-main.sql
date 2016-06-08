fc767de6c848fa242d95dfe95ec3b6311720a659
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-06-07 22:02:42 -0700

    Add scripts.

--- basic_sql_playground/6-main.sql
+++ basic_sql_playground/6-main.sql
@@ -11,7 +11,150 @@ FROM tmp_TVShow;
 
 DROP TABLE tmp_TVShow;
 
-SELECT name, age FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+CREATE TABLE age_sum (
+  name TEXT,
+  age_sum INTEGER
+);
+
+CREATE TABLE min_age (
+  name TEXT,
+  first_name TEXT,
+  last_name TEXT,
+  min_age INTEGER
+);
+
+INSERT INTO age_sum(name, age_sum)
+SELECT "Game of Thrones", (SELECT sum(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
       ON Person.id = TVShowPerson.person_id
       AND EyesColor.person_id = Person.id
-      AND TVShowPerson.tvshow_id = TVShow.person_id_for_show;
+      AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+      WHERE
+        TVShow.name = 'Game of Thrones');
+
+INSERT INTO age_sum(name, age_sum)
+SELECT "Breaking bad", (SELECT sum(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+      ON Person.id = TVShowPerson.person_id
+      AND EyesColor.person_id = Person.id
+      AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+      WHERE
+        TVShow.name = 'Breaking bad');
+
+INSERT INTO age_sum(name, age_sum)
+SELECT "The big bang theory", (SELECT sum(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+      ON Person.id = TVShowPerson.person_id
+      AND EyesColor.person_id = Person.id
+      AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+      WHERE
+        TVShow.name = 'The big bang theory');
+
+INSERT INTO min_age(name, first_name, last_name, min_age)
+SELECT "Game of Thrones",
+(SELECT first_name FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+        ON Person.id = TVShowPerson.person_id
+        AND EyesColor.person_id = Person.id
+        AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+        WHERE
+          TVShow.name = 'Game of Thrones'
+          AND Person.age = (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+              ON Person.id = TVShowPerson.person_id
+              AND EyesColor.person_id = Person.id
+              AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+              WHERE
+                TVShow.name = 'Game of Thrones')),
+
+    (SELECT last_name FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+        ON Person.id = TVShowPerson.person_id
+        AND EyesColor.person_id = Person.id
+        AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+        WHERE
+          TVShow.name = 'Game of Thrones'
+          AND Person.age = (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+              ON Person.id = TVShowPerson.person_id
+              AND EyesColor.person_id = Person.id
+              AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+              WHERE
+                TVShow.name = 'Game of Thrones')),
+
+    (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+        ON Person.id = TVShowPerson.person_id
+        AND EyesColor.person_id = Person.id
+        AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+        WHERE
+          TVShow.name = 'Game of Thrones');
+
+
+INSERT INTO min_age(name, first_name, last_name, min_age)
+SELECT "Breaking bad",
+(SELECT first_name FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+        ON Person.id = TVShowPerson.person_id
+        AND EyesColor.person_id = Person.id
+        AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+        WHERE
+          TVShow.name = 'Breaking bad'
+          AND Person.age = (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+              ON Person.id = TVShowPerson.person_id
+              AND EyesColor.person_id = Person.id
+              AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+              WHERE
+                TVShow.name = 'Breaking bad')),
+
+    (SELECT last_name FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+        ON Person.id = TVShowPerson.person_id
+        AND EyesColor.person_id = Person.id
+        AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+        WHERE
+          TVShow.name = 'Breaking bad'
+          AND Person.age = (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+              ON Person.id = TVShowPerson.person_id
+              AND EyesColor.person_id = Person.id
+              AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+              WHERE
+                TVShow.name = 'Breaking bad')),
+
+      (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+          ON Person.id = TVShowPerson.person_id
+          AND EyesColor.person_id = Person.id
+          AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+          WHERE
+            TVShow.name = 'Breaking bad');
+
+INSERT INTO min_age(name, first_name, last_name, min_age)
+SELECT "The big bang theory",
+  (SELECT first_name FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+          ON Person.id = TVShowPerson.person_id
+          AND EyesColor.person_id = Person.id
+          AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+          WHERE
+            TVShow.name = 'The big bang theory'
+            AND Person.age = (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+                ON Person.id = TVShowPerson.person_id
+                AND EyesColor.person_id = Person.id
+                AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+                WHERE
+                  TVShow.name = 'The big bang theory')),
+
+      (SELECT last_name FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+          ON Person.id = TVShowPerson.person_id
+          AND EyesColor.person_id = Person.id
+          AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+          WHERE
+            TVShow.name = 'The big bang theory'
+            AND Person.age = (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+                ON Person.id = TVShowPerson.person_id
+                AND EyesColor.person_id = Person.id
+                AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+                WHERE
+                  TVShow.name = 'The big bang theory')),
+
+      (SELECT min(age) FROM TVShowPerson, EyesColor, TVShow INNER JOIN Person
+          ON Person.id = TVShowPerson.person_id
+          AND EyesColor.person_id = Person.id
+          AND TVShowPerson.tvshow_id = TVShow.person_id_for_show
+          WHERE
+            TVShow.name = 'The big bang theory');
+
+SELECT * FROM age_sum
+ORDER BY age_sum ASC;
+
+SELECT * FROM min_age
+ORDER BY min_age ASC;
