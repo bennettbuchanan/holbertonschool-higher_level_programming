658525834a04d137683119573ebb06a27fd85d39
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-06-30 17:28:51 -0700

    Add Store class.

--- splitting_tasks_to_be_faster/h_store.py
+++ splitting_tasks_to_be_faster/h_store.py
@@ -1,19 +1,43 @@
-import threading
-import requests
-import json
+from threading import BoundedSemaphore
 import time
 import random
 
 
-class Store(threading.Thread):
-    def __init__(self, item_number, person_capacity):
-        threading.Thread.__init__(self)
+class Store(object):
+    item_number = None
+    people_in_store = None
 
+    def __init__(self, item_number, person_capacity):
+        '''The store class.
+
+        Keyword arguments:
+        item_number -- The number of items that are in the store.
+        person_capacity -- The maximum amount of people allowed in the store.
+        '''
         self.__item_number = item_number
         self.__person_capacity = person_capacity
 
+        if Store.item_number is None:
+            Store.item_number = self.__item_number
+
+        if Store.people_in_store is None:
+            Store.people_in_store = BoundedSemaphore(self.__person_capacity)
+
     def enter(self):
-        if threading.activeCount()
+        '''Calls the acquire method on the BoundedSemaphore incrementor.'''
+        Store.people_in_store.acquire()
+        return
 
     def buy(self):
+        '''Waits between 5 and 10 seconds before calling release on the
+        previous acquire. If the items in the store are 0, return False,
+        otherwise decrement Store.item_number and return True.
+        '''
         time.sleep(random.uniform(5, 10))
+        if Store.item_number == 0:
+            Store.people_in_store.release()
+            return False
+        else:
+            Store.item_number -= 1
+            Store.people_in_store.release()
+            return True
